2779c7e4e9fc251104d72aa53c2ca8f9b9027713
matthewtregg <tregearmatthew@gmail.com>
2019-06-17 18:14:57 +0200

    fix: fetch doesnt work

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -8,15 +8,34 @@ exports.sendPicToFirebase = async (blob, uuid, emailAddress, voteCount) => {
     contentType: 'image/jpeg'
   })
   firebase.storage().ref(uuid + '.jpg').getDownloadURL().then(url => {
+    const photoInfo = {uuid,
+    email: emailAddress,
+    imageUrl: url,
+    voteCount: voteCount}
+    console.log(photoInfo);
+    return photoInfo})
+    .then((photoInfo) =>{ 
+    console.log(photoInfo);
+    return fetch('http://localhost:4000/photo_data',{
+      'method': 'POST',
+      'headers':{ 
+      'content-type': 'application/json'
+      },
+      'body': JSON.stringify(photoInfo)
+    }) .then(res => {
+      console.log('hello')
+      return res.status === 200 ? res : Promise.reject(res)})
+  })
+   
+    .catch((err) => {
+      console.log(`${err.message} while fetching /${url}`)
+    });
 
-    console.log(uuid, emailAddress, url)
-     
-  })  
-  
+}  
 
 
 
 
 
 
-}
+
