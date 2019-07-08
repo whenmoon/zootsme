1e59ce1db2b0a6e6f106f97def2aa18785b57e2f
Tom Belton <33122888+whenmoon@users.noreply.github.com>
2019-07-08 16:44:29 +0200

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+}
+
+const SERVER_BASE_URL = 'http://localhost:4000/';
+
+module.exports = { config, SERVER_BASE_URL };
